d9da68e5fbe173877b359abda927f4f5bb3446f2
Leo <goldcarrotminer@gmail.com>
2022-01-08 01:47:03 +0800

    Add accessControlAllowOrigin headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
