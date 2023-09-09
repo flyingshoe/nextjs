f6d2db683eeeacd0d0d3530399f1373e7ddee1d9
Leo <goldcarrotminer@gmail.com>
2023-09-09 11:39:51 +0800

    feat: change home page to a directory

--- src/constants/navbar.js
+++ src/constants/navbar.js
@@ -1,5 +1,9 @@
 export const navItems = [
-  { title: "Job Search", path: "/jobSearch" },
+  {
+    title: "Job Search",
+    path: "/jobSearch",
+    desc: "MyCareersFuture's APIs have very specific search keywords, thus this app allows you to call multiple APIs with different keywords and packages it in a much nicer UI =)",
+  },
   // { title: "React Query", path: "/jobSearch/reactQuery" },
   // { title: "SSG", path: "/jobSearch/ssg" },
   // { title: "ISR", path: "/jobSearch/isr" },
