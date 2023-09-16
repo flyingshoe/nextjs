70ab1e4026dd79020907045e914ace176d63e492
Leo <goldcarrotminer@gmail.com>
2023-09-16 15:53:57 +0800

    fix: modify page title

---
+++ src/app/jobSearch/layout.tsx
@@ -0,0 +1,9 @@
+import { NextLayout } from "@/types/common";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Find Job!!!",
+};
+export default function Layout({ children }: NextLayout) {
+  return children;
+}
