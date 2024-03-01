a1f52e1a0daa280b8dba2bf62d24b172a7a643d3
Leo <goldcarrotminer@gmail.com>
2024-03-02 00:33:00 +0800

    fix: remove an exclamation mark to check it

--- src/app/jobSearch/layout.tsx
+++ src/app/jobSearch/layout.tsx
@@ -2,7 +2,7 @@ import { NextLayout } from "@/types/common";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Find Job!!!",
+  title: "Find Job!!",
 };
 export default function Layout({ children }: NextLayout) {
   return children;
