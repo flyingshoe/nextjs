00d06aae0e15211e82b8104e8cfa8bb5c9ce24a8
LEORIC\leoric <goldcarrotminer@gmail.com>
2026-08-23 10:50:28 +0800

    fix: remove nextpwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-});
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -26,4 +22,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
